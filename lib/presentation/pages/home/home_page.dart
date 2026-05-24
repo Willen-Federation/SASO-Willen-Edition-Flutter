caebb54fd445d6919e87546f2f70abd41c3d411b
kackey621 <a.kusama@icloud.com>
2026-05-24 15:17:37 +0900

    fix(home): restore colorScheme local var

--- lib/presentation/pages/home/home_page.dart
+++ lib/presentation/pages/home/home_page.dart
@@ -13,6 +13,7 @@ class HomePage extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final config = ref.watch(serverConfigNotifierProvider);
     final isMock = config.apiMode == ApiMode.mock;
+    final colorScheme = Theme.of(context).colorScheme;
 
     final pendingCountAsync = ref.watch(pendingCountProvider);
     final pendingCount = pendingCountAsync.when(
